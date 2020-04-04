5833b2d489d8e6330de3934602f026622a60e870
Erwin Bonsma <eriban@users.sourceforge.net>
2020-04-04 21:16:44 +0200

    Initial commit of TuneGenerator
    It supports only triangle waves and no effects.
    It has not yet tested.

---
+++ MusicGB/ChannelTune.h
@@ -0,0 +1,82 @@
+//
+//  ChannelTune.h
+//  MusicGB
+//
+//  Created by Erwin on 04/04/2020.
+//  Copyright © 2020 Erwin. All rights reserved.
+//
+
+#ifndef ChannelTune_h
+#define ChannelTune_h
+
+#include <stdio.h>
+
+constexpr int sampleRate = 22050;
+constexpr int samplesPerTick = 90 * (sampleRate / 11025);
+
+constexpr int numNotes = 12;
+enum class Note {
+    A, As, B, C, Cs, D, Ds, E, F, Fs, G, Gs
+};
+
+enum class WaveForm {
+    TRIANGLE,
+    TILTED_SAW,
+    SAW,
+    SQUARE,
+    PULSE,
+    ORGAN
+};
+
+enum class Effect {
+    NONE,
+    SLIDE,
+    VIBRATO,
+    DROP,
+    FADE_IN,
+    FADE_OUT
+};
+
+struct NoteSpec {
+    Note note;
+    int oct; // [2..6]
+    int vol; // [1..8]
+    WaveForm wav;
+    Effect fx;
+};
+
+struct TuneSpec {
+    int noteDuration;        // in "ticks". [1..64]
+    int loopStart, loopEnd;
+    const NoteSpec* notes;
+};
+
+struct WaveTable {
+    const int numSamples;
+    const int* samples;
+};
+
+class TuneGenerator {
+    const TuneSpec* _tuneSpec;
+    int _samplesPerNote;
+    const NoteSpec* _note;
+    const NoteSpec* _lastNote;
+    const NoteSpec* _resumeNote;
+
+// Current note
+    const WaveTable* _waveTable;
+    int _volumeStart, _volumeEnd;
+    int _indexDeltaStart, _indexDeltaEnd;
+    int _sampleIndex, _maxSampleIndex;
+    int _waveIndex, _maxWaveIndex;
+
+    void startNote();
+
+public:
+    void setTuneSpec(const TuneSpec* tuneSpec);
+
+    // Returns the number of samples added
+    int addSamples(int* buf, int maxSamples);
+};
+
+#endif /* ChannelTune_h */
--- MusicGB/main.cpp
+++ MusicGB/main.cpp
@@ -8,8 +8,37 @@
 
 #include <iostream>
 
+#include "ChannelTune.h"
+
+const TuneSpec testTune = TuneSpec {
+    .noteDuration = 8,
+    .loopStart = 8,
+    .loopEnd = 8,
+    .notes = new NoteSpec[8] {
+        NoteSpec { .note=Note::A, .oct=4, .vol=8, .wav=WaveForm::TRIANGLE, .fx=Effect::NONE },
+        NoteSpec { .note=Note::B, .oct=4, .vol=8, .wav=WaveForm::TRIANGLE, .fx=Effect::NONE },
+        NoteSpec { .note=Note::C, .oct=4, .vol=8, .wav=WaveForm::TRIANGLE, .fx=Effect::NONE },
+        NoteSpec { .note=Note::D, .oct=4, .vol=8, .wav=WaveForm::TRIANGLE, .fx=Effect::NONE },
+        NoteSpec { .note=Note::E, .oct=4, .vol=8, .wav=WaveForm::TRIANGLE, .fx=Effect::NONE },
+        NoteSpec { .note=Note::F, .oct=4, .vol=8, .wav=WaveForm::TRIANGLE, .fx=Effect::NONE },
+        NoteSpec { .note=Note::G, .oct=4, .vol=8, .wav=WaveForm::TRIANGLE, .fx=Effect::NONE },
+        NoteSpec { .note=Note::A, .oct=4, .vol=8, .wav=WaveForm::TRIANGLE, .fx=Effect::NONE }
+    }
+};
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    TuneGenerator tuneGen;
+
+    tuneGen.setTuneSpec(&testTune);
+    int buf[512];
+    int samplesAdded;
+    do {
+        samplesAdded = tuneGen.addSamples(buf, 512);
+        for (int i = 0; i < samplesAdded; i++) {
+            std::cout << buf[i] << "\n";
+        }
+    } while (0 && samplesAdded == 512);
+
     return 0;
 }
