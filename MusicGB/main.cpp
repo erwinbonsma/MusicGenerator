6319a23f2471a12db6ec23d00a35b3f247559f36
Erwin Bonsma <eriban@users.sourceforge.net>
2020-04-04 13:30:28 +0200

    Initial Commit

---
+++ MusicGB/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MusicGB
+//
+//  Created by Erwin on 04/04/2020.
+//  Copyright © 2020 Erwin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
